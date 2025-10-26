f8330abe331143d7b85bebcb210e440ce3a061d2
palnaz <sierra7seven@gmail.com>
2025-10-26 15:58:34 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/naufal-portfolio",
+  base: "/naufal-portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
